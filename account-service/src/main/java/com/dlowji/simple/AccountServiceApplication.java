a573945e0284f4e86c314522b570f690f9881e66
S1mpleOW <dungdqch@gmail.com>
2023-03-31 10:40:50 +0700

    update command line runner

--- account-service/src/main/java/com/dlowji/simple/AccountServiceApplication.java
+++ account-service/src/main/java/com/dlowji/simple/AccountServiceApplication.java
@@ -1,11 +1,68 @@
 package com.dlowji.simple;
 
+import com.dlowji.simple.command.api.data.IAccountRepository;
+import com.dlowji.simple.command.api.data.IRoleRepository;
+import com.dlowji.simple.command.api.data.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class AccountServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AccountServiceApplication.class, args);
-    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+	@Component
+	@Order(1)
+	class setupRole implements CommandLineRunner {
+		@Autowired
+		private IRoleRepository iRoleRepository;
+
+		@Override
+		public void run(String... args) throws Exception {
+			try {
+				if (!iRoleRepository.findById("R01").isPresent()) {
+					Role adminRole = Role.builder().roleId("R01").roleName("Admin").description("Super power role, the role can " + "defeat anyone like @Dlowji").build();
+					iRoleRepository.save(adminRole);
+				}
+
+				if (!iRoleRepository.findById("RO2").isPresent()) {
+					Role employeeRole = Role.builder().roleId("R02").roleName("Employee").description("Chicken role").build();
+					iRoleRepository.save(employeeRole);
+				}
+
+				if (!iRoleRepository.findById("R03").isPresent()) {
+					Role chefRole = Role.builder().roleId("R03").roleName("Chef").description("Powerful role")
+					.build();
+					iRoleRepository.save(chefRole);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Component
+	@Order(2)
+	class setupAccount implements CommandLineRunner {
+		@Autowired
+		private IAccountRepository iAccountRepository;
+
+		@Override
+		public void run(String... args) throws Exception {
+			try {
+			}
+			catch (Exception e) {
+
+			}
+
+		}
+	}
 }
--- kitchen-service/src/main/java/com/dlowji/simple/KitchenServiceApplication.java
+++ kitchen-service/src/main/java/com/dlowji/simple/KitchenServiceApplication.java
@@ -1,11 +1,59 @@
 package com.dlowji.simple;
 
+import com.dlowji.simple.command.api.data.Category;
+import com.dlowji.simple.command.api.data.ICategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class KitchenServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(KitchenServiceApplication.class, args);
-    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenServiceApplication.class, args);
+	}
+
+	@Component
+	@Order(1)
+	class setupCategory implements CommandLineRunner {
+		@Autowired
+		private ICategoryRepository iCategoryRepository;
+		@Override
+		public void run(String... args) throws Exception {
+			try{
+				if (!iCategoryRepository.findById("C01").isPresent()) {
+					Category drink = Category.builder().categoryId("C01").categoryName("Drink").link("?q=drink").icon("fas fa-cocktail").build();
+					iCategoryRepository.save(drink);
+				}
+
+				if (!iCategoryRepository.findById("C02").isPresent()) {
+					Category desert = Category.builder().categoryId("C02").categoryName("Dessert").link("?q=dessert").icon("fas fa-cookie-bite").build();
+					iCategoryRepository.save(desert);
+				}
+
+				if (!iCategoryRepository.findById("C03").isPresent()) {
+					Category pizza = Category.builder().categoryId("C03").categoryName("Pizza").link("?q=pizza").icon("fas fa-pizza-slice").build();
+					iCategoryRepository.save(pizza);
+				}
+
+				if (!iCategoryRepository.findById("C04").isPresent()) {
+					Category hamburger = Category.builder().categoryId("C04").categoryName("Hamburger").link("?q=hamburger").icon("fas fa-hamburger").build();
+					iCategoryRepository.save(hamburger);
+				}
+
+				if (!iCategoryRepository.findById("C05").isPresent()) {
+					Category pasta = Category.builder().categoryId("C05").categoryName(
+					"Pasta").icon("fa-solid fas fa-pastafarianism").link("?q=pasta").build();
+					iCategoryRepository.save(pasta);
+				}
+			}
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
